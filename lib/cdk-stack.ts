bc1d447a9099731b602b993f2bf279c02c8a4273
Iris Birnabum <iris.birnbaum@baaderbank.de>
2025-03-27 15:30:56 +0100

    feat: baader setup

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -11,8 +11,6 @@ export class CdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // const vpc = new ec2.Vpc(this, 'EKS-VPC', { maxAzs: 2 }); //to create vpc
-
-
     // find existing vpc
     const vpc = ec2.Vpc.fromLookup(this, 'ExistingVPC', {
       vpcId: 'vpc-xxxxxxxx' // Replace VPC ID
@@ -25,43 +23,61 @@ export class CdkStack extends cdk.Stack {
     });
 
     eksSecurityGroup.addIngressRule(
-      ec2.Peer.ipv4('x.x.x.x/24'),  
+      ec2.Peer.ipv4('10.15.4.11/32'),  
       ec2.Port.tcp(443),  
       'Allow on-prem node CIDR range'
     );
 
     eksSecurityGroup.addIngressRule(
-      ec2.Peer.ipv4('x.x.x.x/24'),  
+      ec2.Peer.ipv4('10.15.4.12/32'),  
       ec2.Port.tcp(443),  
       'Allow on-prem pod CIDR range'
     );
+
+    eksSecurityGroup.addIngressRule(
+      ec2.Peer.ipv4('10.15.4.13/32'),  
+      ec2.Port.tcp(443),  
+      'Allow on-prem pod CIDR range'
+    );
+
+    const endpointSecurityGroup = new ec2.SecurityGroup(this, 'EndpointSG', {
+      vpc,
+      description: 'Security group for VPC endpoints',
+      allowAllOutbound: true, // Ensure outbound traffic is allowed
+    });
+
+    /*const eksEndpoint = vpc.addInterfaceEndpoint('EKSServiceEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.EKS,
+      privateDnsEnabled: true,
+      securityGroups: [endpointSecurityGroup],
+    });*/
 
     const cluster = new eks.Cluster(this, 'obs', {
       version: eks.KubernetesVersion.V1_32,
       kubectlLayer: new KubectlV32Layer(this, 'kubectl'),
       clusterName: "obs-test",
       securityGroup: eksSecurityGroup, 
+      placeClusterHandlerInVpc: true,
+      clusterHandlerSecurityGroup: endpointSecurityGroup,
+      endpointAccess: eks.EndpointAccess.PRIVATE,
       defaultCapacity: 0, // prevent CDK from creating default worker nodes 
       vpc,
-
-      //to deploy cluster in private subnets
+      authenticationMode: eks.AuthenticationMode.API_AND_CONFIG_MAP,
+      //to deploy cluster in private subnets<
       vpcSubnets: [
         {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED, 
         },
       ],
       remoteNodeNetworks: [
-        {cidrs: ["x.x.x.x/24"]} //On-prem node cidr range, must for hybrid nodes
+        {cidrs: ["10.15.4.11/32", "10.15.4.12/32", "10.15.4.13/32"]} //On-prem node cidr range, must for hybrid nodes
       ],
-      remotePodNetworks: [
-        {cidrs: ["x.x.x.x/24"]} //On-prem pod cidr range
-      ]
     });
-
 
     // add sso roles here
     const ssoRoles = [
-      'arn:aws:iam::<ACC ID>:role/AWSReservedSSO_AdministratorAccess_596e9bb8690dba1d',
+      'arn:aws:iam::xxx:role/xxx',
+      'arn:aws:iam::xxx:role/xxx'
     ];
 
     ssoRoles.forEach((roleArn, index) => {
@@ -82,22 +98,22 @@ export class CdkStack extends cdk.Stack {
     //   release: 'metrics-server',
     // });
 
-    cluster.addHelmChart('cilium', {
-      chart: 'cilium',
-      repository: 'https://helm.cilium.io/',
-      release: 'cilium',
-      version: '1.17.2',
-      namespace: 'kube-system',
-      values: {
-        'cni': { 'exclusive': true },
-        'ipam': { 'mode': 'cluster-pool' },
-        'bpf': { 'masquerade': true },
-        'nodePort': {'enabled': true},
-        'clusterPoolIPv4PodCIDRList': ["10.20.0.0/16"] //change CIDR
-        // 'routingMode': 'native',
-        // 'ipv4NativeRoutingCIDR': "10.11.0.0/16",
-      }
-    });
+    // cluster.addHelmChart('cilium', {
+    //   chart: 'cilium',
+    //   repository: 'https://helm.cilium.io/',
+    //   release: 'cilium',
+    //   version: '1.17.2',
+    //   namespace: 'kube-system',
+    //   values: {
+    //     'cni': { 'exclusive': true },
+    //     'ipam': { 'mode': 'cluster-pool' },
+    //     'bpf': { 'masquerade': true },
+    //     'nodePort': {'enabled': true},
+    //     'clusterPoolIPv4PodCIDRList': ["10.20.0.0/16"] //change CIDR
+    //     // 'routingMode': 'native',
+    //     // 'ipv4NativeRoutingCIDR': "10.11.0.0/16",
+    //   }
+    // });
 
     // new eks.Addon(this, 'ebs-csi', {
     //   cluster,
